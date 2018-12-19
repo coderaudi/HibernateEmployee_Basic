ebaec049acc9ffd9f091baf3dd7c627975070628
ABHIJEET KHIRE <coderaudi@users.noreply.github.com>
2018-12-19 23:26:48 +0530

    Add files via upload

---
+++ EmployeeHibernatePro/src/com/audi/pkg1/User.java
@@ -0,0 +1,49 @@
+package com.audi.pkg1;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Transient;
+
+@Entity(name = "User_Details")
+public class User {
+
+	@Id
+	@Column(name = "UserID")
+	private int id;
+
+	@Column(name = "UseName")
+	private String name;
+
+	@Transient
+	private String country = "India";
+
+	@Lob
+	private String bio;
+
+	public int getId() {
+		return id;
+	}
+
+	public String getBio() {
+		return bio;
+	}
+
+	public void setBio(String bio) {
+		this.bio = bio;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
